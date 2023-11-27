146093beb5b9e6c495e588c87778d68155432a0a
WONDO-k <wondok95@naver.com>
2023-11-27 17:28:45 +0900

    [feat] Api Error Code 적용

--- service/api/src/main/java/org/delivery/api/common/api/Api.java
+++ service/api/src/main/java/org/delivery/api/common/api/Api.java
@@ -3,8 +3,10 @@ package org.delivery.api.common.api;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 import javax.validation.Valid;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -22,4 +24,27 @@ public class Api<T> {
         api.body = data;
         return api;
     }
+
+    // 에러가 났을 때 바디에 세팅할 것이 없기 때문에 제레릭의 경고를 없애기 위해 오브젝트로 세팅함
+    public static Api<Object> ERROR(Result result) {
+        var api = new Api<Object>();
+        api.result = result;
+        return api;
+    }
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs) {
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs);
+        return api;
+    }
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs, Throwable tx) {
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs,tx);
+        return api;
+    }
+    public static Api<Object> ERROR(ErrorCodeIfs errorCodeIfs,String description) {
+        var api = new Api<Object>();
+        api.result = Result.ERROR(errorCodeIfs,description);
+        return api;
+    }
+
 }
--- service/api/src/main/java/org/delivery/api/common/api/Result.java
+++ service/api/src/main/java/org/delivery/api/common/api/Result.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.ErrorCodeIfs;
 
 @Data
 @NoArgsConstructor
@@ -19,10 +21,32 @@ public class Result {
 
     public static Result ok(){
         return Result.builder()
-                .resultCode(200)
-                .resultMessage("OK")
+                .resultCode(ErrorCode.OK.getErrorCode())
+                .resultMessage(ErrorCode.OK.getDescription())
                 .resultDescription("성공")
                 .build();
     }
 
+    public static Result ERROR(ErrorCodeIfs errorCodeIfs){
+        return Result.builder()
+                .resultCode(ErrorCode.OK.getErrorCode())
+                .resultMessage(ErrorCode.OK.getDescription())
+                .resultDescription("성공")
+                .build();
+    }
+    public static Result ERROR(ErrorCodeIfs errorCodeIfs,Throwable tx){
+        return Result.builder()
+                .resultCode(ErrorCode.OK.getErrorCode())
+                .resultMessage(ErrorCode.OK.getDescription())
+                .resultDescription(tx.getLocalizedMessage()) // 추후 수정할 듯
+                .build();
+    }
+
+    public static Result ERROR(ErrorCodeIfs errorCodeIfs,String description){
+        return Result.builder()
+                .resultCode(errorCodeIfs.getErrorCode())
+                .resultMessage(errorCodeIfs.getDescription())
+                .resultDescription(description)
+                .build();
+    }
 }
